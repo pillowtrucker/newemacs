8bc684d15f02fc366c30553524d22e8777134dc3
pillowtrucker <githubino@janitor.chat>
2023-12-10 13:23:55 +0100

    find a dap-mode that actually bloody works

--- custom.el
+++ custom.el
@@ -276,20 +276,52 @@
   :commands (helm-lsp-workspace-symbol helm-lsp-code-actions)
   )
 (keymap-set lsp-command-map "<remap> <lsp-execute-code-action>" 'helm-lsp-code-actions)
-(use-package dap-mode
-  :defer
-  :custom
-  (dap-auto-configure-mode t                           "Automatically configure dap.")
-  (dap-auto-configure-features
-   '(sessions locals breakpoints expressions tooltip)  "Remove the button panel in the top.")
-  :hook
-  ((rustic-mode . dap-mode) (c++-mode . dap-mode))
+;;(use-package dap-mode
+;;  :defer
+;;  :custom
+;;  (dap-auto-configure-mode t                           "Automatically configure dap.")
+;;  (dap-auto-configure-features
+ ;;  '(sessions locals breakpoints expressions tooltip)  "Remove the button panel in the top.")
+;;  :hook
+;;  ((rustic-mode . dap-mode) (c++-mode . dap-mode))
+;;  :config
+;;  (require 'dap-lldb)
+;;  (require 'dap-cpptools)
+;;  
+;;  )
+(use-package dape
+  :straight (:repo "/svaante/dape"
+                   :host github
+                   :type git)
+  ;; To use window configuration like gud (gdb-mi)
+   :init
+   (setq dape-buffer-window-arrangment 'gud)
   :config
-  (require 'dap-lldb)
-  (require 'dap-cpptools)
-  
-  )
+  ;; Info buffers to the right
+  ;; (setq dape-buffer-window-arrangment 'right)
 
+  ;; To not display info and/or buffers on startup
+  ;; (remove-hook 'dape-on-start-hooks 'dape-info)
+  ;; (remove-hook 'dape-on-start-hooks 'dape-repl)
+
+  ;; To display info and/or repl buffers on stopped
+   (add-hook 'dape-on-stopped-hooks 'dape-info)
+   (add-hook 'dape-on-stopped-hooks 'dape-repl)
+
+  ;; By default dape uses gdb keybinding prefix
+   (setq dape-key-prefix "\C-x\C-a")
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-compile-hooks 'kill-buffer)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-on-start-hooks
+  ;;           (defun dape--save-on-start ()
+  ;;             (save-some-buffers t t)))
+
+  ;; Projectile users
+   (setq dape-cwd-fn 'projectile-project-root)
+  )
 
 
 (use-package minions
